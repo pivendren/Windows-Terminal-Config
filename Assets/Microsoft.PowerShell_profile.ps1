397113ebcc89d47d5e0cad0357ca426a3a76d4e3
Pivendren Naik <pivendren@gmail.com>
2020-09-13 00:22:10 +0200

    Hide username@hostname

--- Assets/Microsoft.PowerShell_profile.ps1
+++ Assets/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,4 @@
 Import-Module posh-git
 Import-Module oh-my-posh
-Set-Theme Agnoster
+Set-Theme Paradox
+$DefaultUser = $env:UserName #Hide your username@domain when not in a virtual machine for the Agnoster
